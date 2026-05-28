31e70160f9be2409399229ebe6c11af30c979d85
sumit2898 <kumarsumit272006gmail.com>
2026-05-28 19:24:48 +0530

    customreact

---
+++ 01vitebasic/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+
+import Sumit from './Sumit'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>// this is a fragment
+      <Sumit />
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+   
+  )
+}
+
+export default App
---
+++ 01vitebasic/src/Sumit.jsx
@@ -0,0 +1,9 @@
+function Sumit() {
+    return (
+        <div>
+            <h1>this is Sumit</h1>
+        </div>
+    )
+}
+
+export default Sumit;
