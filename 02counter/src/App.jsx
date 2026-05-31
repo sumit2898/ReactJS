93fafbb62940c87f938b6a0db4d651bf5d3c98ee
sumit2898 <kumarsumit272006gmail.com>
2026-05-31 11:33:51 +0530

    props

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter]  = useState(15)
+
+  //let counter = 15
+
+  const addValue = () => {
+    //counter = counter + 1
+    if( counter < 20){
+      setCounter(counter + 1)
+    } else {
+      console.log("Greater than 20");
+    }
+  }
+
+  const removeValue = () => {
+    if (counter > 0){
+      setCounter(counter - 1)
+    } else {
+      console.log("negative numbers")
+    }
+  }
+
+  return (
+    <>
+      <h1>React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value {counter}</button> 
+      <br />
+      <button
+      onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
